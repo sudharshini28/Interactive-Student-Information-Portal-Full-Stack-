289b4c7d98378ae3d45f7c1e5f4e5713c4ed4599
sudharshini28 <sudharshiniarumugam12@gmail.com>
2025-12-12 15:07:19 +0530

    Disabled JPA and DB auto configuration for Render deployment

---
+++ src/main/java/com/student/app/NoRepositoryConfig.java
@@ -0,0 +1,10 @@
+package com.student.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "none")
+public class NoRepositoryConfig {
+
+}
--- src/main/java/com/student/app/StudentPortalBackendApplication.java
+++ src/main/java/com/student/app/StudentPortalBackendApplication.java
@@ -3,8 +3,14 @@ package com.student.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class
+        }
+)
 public class StudentPortalBackendApplication {
 
     public static void main(String[] args) {
